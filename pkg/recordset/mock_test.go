ec160c77cbeffc9721b7d409fe4cba5d6ca06330
Théo Brigitte <theo.brigitte@gmail.com>
2018-10-18 09:39:28 +0200

    Implement mock client logic (#10)

--- pkg/recordset/mock_test.go
+++ pkg/recordset/mock_test.go
@@ -1,50 +1,206 @@
 package recordset
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cloudformation"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/elb"
 )
 
-type sourceClientMock struct{}
-
-func (s *sourceClientMock) DescribeStacks(*cloudformation.DescribeStacksInput) (*cloudformation.DescribeStacksOutput, error) {
-	return nil, nil
+type sourceClientMock struct {
+	sourceStacks []cloudformation.Stack
 }
 
-func (s *sourceClientMock) ListStacks(*cloudformation.ListStacksInput) (*cloudformation.ListStacksOutput, error) {
-	return nil, nil
+func newSourceWithStacks(stacks []cloudformation.Stack) *sourceClientMock {
+	return &sourceClientMock{
+		sourceStacks: stacks,
+	}
+}
+
+func (s *sourceClientMock) DescribeStacks(input *cloudformation.DescribeStacksInput) (*cloudformation.DescribeStacksOutput, error) {
+	if s == nil || input == nil || input.StackName == nil {
+		return nil, mockClientError
+	}
+
+	for _, stack := range s.sourceStacks {
+		if stack.StackName != nil && *stack.StackName == *input.StackName {
+			output := &cloudformation.DescribeStacksOutput{
+				Stacks: []*cloudformation.Stack{
+					&stack,
+				},
+			}
+
+			return output, nil
+		}
+	}
+
+	return nil, mockClientError
+}
+
+func (s *sourceClientMock) ListStacks(input *cloudformation.ListStacksInput) (*cloudformation.ListStacksOutput, error) {
+	if s == nil {
+		return nil, mockClientError
+	}
+
+	filters := []string{}
+	if input != nil {
+		for _, f := range input.StackStatusFilter {
+			if f != nil {
+				filters = append(filters, *f)
+			}
+		}
+	}
+
+	output := &cloudformation.ListStacksOutput{}
+	for _, stack := range s.sourceStacks {
+		add := false
+		if len(filters) > 0 && stack.StackStatus != nil {
+			for _, f := range filters {
+				if f == *stack.StackStatus {
+					add = true
+				}
+			}
+		} else {
+			add = true
+		}
+
+		if add {
+			s := &cloudformation.StackSummary{
+				StackId:     stack.StackName,
+				StackName:   stack.StackName,
+				StackStatus: stack.StackStatus,
+			}
+			output.StackSummaries = append(output.StackSummaries, s)
+		}
+	}
+
+	return output, nil
 }
 
 func (s *sourceClientMock) DescribeInstances(*ec2.DescribeInstancesInput) (*ec2.DescribeInstancesOutput, error) {
-	return nil, nil
+	output := &ec2.DescribeInstancesOutput{
+		Reservations: []*ec2.Reservation{
+			&ec2.Reservation{
+				Instances: []*ec2.Instance{
+					&ec2.Instance{
+						PrivateDnsName: aws.String("ec2.dns.test"),
+					},
+				},
+			},
+		},
+	}
+	return output, nil
 }
 func (s *sourceClientMock) DescribeLoadBalancers(*elb.DescribeLoadBalancersInput) (*elb.DescribeLoadBalancersOutput, error) {
-	return nil, nil
+	output := &elb.DescribeLoadBalancersOutput{
+		LoadBalancerDescriptions: []*elb.LoadBalancerDescription{
+			&elb.LoadBalancerDescription{
+				DNSName: aws.String("elb.dns.test"),
+			},
+		},
+	}
+
+	return output, nil
 }
 
 type targetClientMock struct {
+	createdStacks []string
 	deletedStacks []string
+	updatedStacks []string
+	targetStacks  []cloudformation.Stack
 }
 
-func (s *targetClientMock) DescribeStacks(*cloudformation.DescribeStacksInput) (*cloudformation.DescribeStacksOutput, error) {
-	return nil, nil
+func newTargetWithStacks(stacks []cloudformation.Stack) *targetClientMock {
+	return &targetClientMock{
+		targetStacks: stacks,
+	}
+}
+func (t *targetClientMock) DescribeStacks(input *cloudformation.DescribeStacksInput) (*cloudformation.DescribeStacksOutput, error) {
+	if t == nil || input == nil || input.StackName == nil {
+		return nil, mockClientError
+	}
+
+	for _, stack := range t.targetStacks {
+		if stack.StackName != nil && *stack.StackName == *input.StackName {
+			output := &cloudformation.DescribeStacksOutput{
+				Stacks: []*cloudformation.Stack{
+					&stack,
+				},
+			}
+
+			return output, nil
+		}
+	}
+
+	return nil, mockClientError
 }
 
-func (t *targetClientMock) ListStacks(*cloudformation.ListStacksInput) (*cloudformation.ListStacksOutput, error) {
-	return nil, nil
+func (t *targetClientMock) ListStacks(input *cloudformation.ListStacksInput) (*cloudformation.ListStacksOutput, error) {
+	if t == nil {
+		return nil, mockClientError
+	}
+
+	filters := []string{}
+	if input != nil {
+		for _, f := range input.StackStatusFilter {
+			if f != nil {
+				filters = append(filters, *f)
+			}
+		}
+	}
+
+	output := &cloudformation.ListStacksOutput{}
+	for _, stack := range t.targetStacks {
+		add := false
+		if len(filters) > 0 && stack.StackStatus != nil {
+			for _, f := range filters {
+				if f == *stack.StackStatus {
+					add = true
+				}
+			}
+		} else {
+			add = true
+		}
+
+		if add {
+			s := &cloudformation.StackSummary{
+				StackId:     stack.StackName,
+				StackName:   stack.StackName,
+				StackStatus: stack.StackStatus,
+			}
+			output.StackSummaries = append(output.StackSummaries, s)
+		}
+	}
+
+	return output, nil
 }
 
-func (t *targetClientMock) CreateStack(*cloudformation.CreateStackInput) (*cloudformation.CreateStackOutput, error) {
+func (t *targetClientMock) CreateStack(input *cloudformation.CreateStackInput) (*cloudformation.CreateStackOutput, error) {
+	if input == nil || input.StackName == nil {
+		return nil, mockClientError
+	}
+
+	t.createdStacks = append(t.createdStacks, *input.StackName)
+
 	return nil, nil
 }
 
 func (t *targetClientMock) DeleteStack(input *cloudformation.DeleteStackInput) (*cloudformation.DeleteStackOutput, error) {
+	if input == nil || input.StackName == nil {
+		return nil, mockClientError
+	}
+
 	t.deletedStacks = append(t.deletedStacks, *input.StackName)
 
 	return nil, nil
 }
 
-func (t *targetClientMock) UpdateStack(*cloudformation.UpdateStackInput) (*cloudformation.UpdateStackOutput, error) {
+func (t *targetClientMock) UpdateStack(input *cloudformation.UpdateStackInput) (*cloudformation.UpdateStackOutput, error) {
+	if input == nil || input.StackName == nil {
+		return nil, mockClientError
+	}
+
+	t.updatedStacks = append(t.updatedStacks, *input.StackName)
+
 	return nil, nil
 }
